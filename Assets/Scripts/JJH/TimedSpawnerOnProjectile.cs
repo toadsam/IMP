0665eb1577819e440cbe0752b3b64c006ec6df73
toadsam <toadsam@naver.com>
2025-04-30 15:34:51 +0900

    [ADD] 폭발 이펙트 추가

---
+++ Assets/Scripts/JJH/TimedSpawnerOnProjectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class TimedSpawnerOnProjectile : MonoBehaviour
+{
+    public GameObject objectToSpawn; // 생성할 물체 (예: 폭발 이펙트 등)
+    public float spawnDelay = 1f;    // 몇 초 뒤에 생성할지
+    public float destroyAfter = 2f;  // 생성된 물체를 몇 초 뒤에 삭제할지
+
+    void Start()
+    {
+        Invoke(nameof(SpawnAtCurrentPosition), spawnDelay);
+    }
+
+    void SpawnAtCurrentPosition()
+    {
+        GameObject spawned = Instantiate(objectToSpawn, transform.position, transform.rotation);
+        Destroy(spawned, destroyAfter);
+    }
+}
